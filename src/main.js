3a3603a99af9f14eac1aa1deae6b97d9a4d3b5ec
Pavel Svitek <svitek.pavel@gmail.com>
2018-08-01 16:17:51 +0200

    Implement disconnecting on successful authentication and 15-min timeout

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
+var connected = false;
+
+function disconnect(client, message) {
+  if (connected === true) {
+    client.disconnect(() => {
+      connected = false;
+
+      if (message) {
+        console.log(message);
+      }
+    });
+  } else {
+    connected = false;
+  }
+}
+
 export function connect(partnerId, scope, onConnect, onUserAuthenticate, API_LOGIN) {
   if (API_LOGIN === undefined) {
     API_LOGIN = 'https://api.authenteq.com/login';
@@ -13,21 +29,35 @@ export function connect(partnerId, scope, onConnect, onUserAuthenticate, API_LOG
   // Don't print debug messages into console
   stompClient.debug = function() {};
 
+  console.log('Authenteq::Connecting..');
   stompClient.connect({}, () => {
     const transportUrl = socket._transport.url; // eslint-disable-line no-underscore-dangle
     const sessionId = /\/([^/]+)\/websocket/.exec(transportUrl)[1];
 
     stompClient.subscribe(`/queue/${sessionId}.authenticationId`, (response) => {
+      console.log('Authenteq::Connected');
+      connected = true;
+
+      // Parse response
       const data = JSON.parse(response.body);
       const tokenId = data.id;
 
       // Handle tokenId to app logic, so app can display a QR code
       onConnect({ tokenId: tokenId, svg: data.svg });
 
+      // Disconnect in 15 minutes automatically
+      setTimeout(
+        () => disconnect(stompClient, 'Authenteq::Disconnected (15 minutes timeout)'),
+        1000 * 60 * 15
+      );
+
       stompClient.subscribe(`/topic/authenticate.${tokenId}`, () => {
 
         // Handle tokenId back to app logic
         onUserAuthenticate(tokenId);
+
+        // Disconnect. Connection is no longer needed.
+        disconnect(stompClient, 'Authenteq::Disconnected (connection no longer needed)');
       });
     });
 
